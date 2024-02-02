78dc64e4d4399209d6f15bc082be5b476fa4ca6e
sbrezo <brezovac.sasa@gmail.com>
2024-02-02 09:04:02 +0100

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Saša Brezovac on 02.02.2024..
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
